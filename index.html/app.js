e3e352b72180a089bb857d221750fd9dd339feed
MUHAMMADfas62398wl <muhammadfaseeh090@gmail.com>
2025-01-01 03:10:00 -0800

    Add files via upload

---
+++ index.html/app.js
@@ -0,0 +1,66 @@
+// console.log("ali")
+// alert("ahmed")
+// document.write("akbar")
+
+
+
+// a=5;
+
+
+
+// // const a=77;
+
+// // let a=7;
+
+// let a=5;
+
+// a=88;
+
+
+// alert(a)
+
+// const b=5;
+
+
+// // b=55;
+// // const b=88;
+
+
+
+// var c=5;
+
+
+// var c=77;
+// c=99;
+
+
+
+
+// let $_firstName1 ="ali"
+// let a=77
+// alert(firstName)
+// alert(a)
+
+// +
+// -
+// /
+// *
+// %
+// =
+// ==
+// ++
+// --
+// **
+
+
+let a=88;
+let b=99;
+
+alert(a+b)
+alert(a-b)
+
+
+let c="ahmed"
+let d="akbar"
+
+alert(c+d)
